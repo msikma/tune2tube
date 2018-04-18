b5e15a6b3356eb5e9cfc19db127e566fa3300ee3
Michiel Sikma <michiel@sikma.org>
2018-04-18 10:42:19 -0700

    Update copyright year to 2018

--- t2t/tunetags.py
+++ t2t/tunetags.py
@@ -2,7 +2,7 @@
 
 # tune2tube.py
 #
-# Copyright (C) 2014-2015 Michiel Sikma and contributors
+# Copyright (C) 2014-2018 Michiel Sikma and contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
